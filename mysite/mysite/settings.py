90ac5c2b95d90ceb99185a4e3b40a6ccef6a8d8e
Hunterito <tymahob@yandex.ru>
2023-07-10 21:53:17 +0300

    ДЗ по 4 занятию

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -35,7 +35,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'app_advertisements'
+    'app_advertisements',
+    'app_lesson_4'
 ]  # установленные приложения
 
 MIDDLEWARE = [
