e55cee08db584a57898a2f00cd3c51ee943e5d20
Hunterito <tymahob@yandex.ru>
2023-08-07 15:30:24 +0300

    Занятие 8

--- mysite/app_advertisements/models.py
+++ mysite/app_advertisements/models.py
@@ -1,5 +1,6 @@
 from django.db import models
-
+from django.contrib import admin
+from django.utils.html import format_html
 # Create your models here.
 
 
@@ -12,6 +13,17 @@ class Advertisement(models.Model):
     updated_at = models.DateTimeField("дата обновления", auto_now=True)
     created_at = models.DateTimeField("дата публикации", auto_now_add=True)
     is_auction = models.BooleanField("уместен ли торг", help_text="отметьте, если торг по бъявлению уместен")
+
+    @admin.display(description="Дата публикации")
+    def created_date(self):
+        from django.utils import timezone
+        if self.created_at.date() == timezone.now().date():
+            created_time = self.created_at.time().strftime("%H:%M:%S")
+            return format_html(
+                "<span>Сегодня в {}</span>", created_time
+            )
+        else:
+            return self.created_at.strftime("%d.%m.%Y - %H:%M")
 
     class Meta:
         db_table = "Advertisements"
